14525a6cd2a6927ecf57676faa6e39ba80c0d803
BenPurdy01 <45369156+BenPurdy01@users.noreply.github.com>
2018-11-26 13:50:53 -0500

    Add files via upload

---
+++ Summative/Main_V01.py
@@ -0,0 +1,35 @@
+# import the pygame module, so you can use it
+import pygame
+WHITE = (255,255,255)
+# define a main function
+def main():
+     
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    pygame.display.set_caption("minimal program")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((240,180))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the eventqueue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+        screen.fill(WHITE)
+        image = pygame.image.load("The_Danny.png")
+        screen.blit(image, (50,50))
+        pygame.display.flip()
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()

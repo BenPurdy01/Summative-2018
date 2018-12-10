99f1ecf29a848bd38efef43e5ae2bcc3e5ce9d44
BenPurdy01 <45369156+BenPurdy01@users.noreply.github.com>
2018-12-10 13:59:10 -0500

    Add files via upload

--- Summative/Main_V04.py
+++ Summative/Main_V04.py
@@ -9,17 +9,22 @@ pygame.display.set_caption("game")
 screen = pygame.display.set_mode((screen_w,screen_h))
 clock = pygame.time.Clock()
 GREEN = (24,92,26)
+RED = (255,0,0)
 
 class Player():
 
     def __init__(self):
         self.x = 50
         self.y = 50
+        self.pos = (self.x,self.y)
         self.sped = 2
         self.Helth = 20
 
     def take_dmg(self):
         self.Helth = self.Helth - 1
+
+    def get_cords(self):
+        return (self.x,self.y)
 
 class Zombie():
 
@@ -34,12 +39,23 @@ class Zombie():
 
 class Bullet():
 
-    def __init__(self):
+    def __init__(self, cords, endpoint):
+        #self.time = 0
         self.sped = 10
-        self.x = 0
-        self.y = 0
+        #make so starts not top left player
+        self.x = cords[0]
+        self.y = cords[1]
         self.w = 1
         self.h = 1
+        
+    def find_line(self):
+        return ((self.endy - self.y) / (self.endx - self.x), self.y)
+
+    def move(self):
+        self.x += self.sped
+        self.y += ((self.endy - self.y) / float(self.endx - self.x)) * self.sped
+        #self.time += 1
+        
 
 
 # define a main function
@@ -51,16 +67,22 @@ def main():
     running = True
     # main loop
     while running:
+        screen.fill(GREEN)
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
             if event.type == pygame.MOUSEMOTION:
                 mousex, mousey = event.pos
                 angle_1 = math.degrees(math.atan2(p.y-mousey, p.x-mousex))+180
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.line(screen, RED, (p.x,p.y), (mousex,mousey), 5)
+                print (mousex,mousey)
+                print "button pressed"
             # only do something if the event is of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
         pressedkeys = pygame.key.get_pressed()
+        pressedmouse = pygame.mouse.get_pressed()
         pygame.key.set_repeat(0,1)
         if pressedkeys[pygame.K_a]:
             if (p.x + 16) > 0:
@@ -82,7 +104,7 @@ def main():
                 p.y += p.sped
             elif (p.y + 56) >= screen_h:
                 p.y = p.y -10
-        screen.fill(GREEN)
+
         image = pygame.image.load("player_noArm.png")
         image = pygame.transform.scale(image, (16,56))
         gun_test = pygame.image.load("M9_v1.png")
@@ -97,7 +119,7 @@ def main():
           gun_test = pygame.transform.rotate(gun_test, -angle_1)
           gun_test = pygame.transform.flip(gun_test, 1,1)
         CH = pygame.image.load("crosshair.png")
-        CH = pygame.transform.scale(CH, (64,64))
+        CH = pygame.transform.scale(CH, (10,10))
         if p.x > zed.x:
             zed.x+=zed.sped
         elif p.x < zed.x:
@@ -106,7 +128,6 @@ def main():
             zed.y+=zed.sped
         elif p.y < zed.y:
             zed.y-=zed.sped
-        print angle_1
         if angle_1 < 270 or angle_1 > 90:
           screen.blit(image, (p.x,p.y))
           screen.blit(gun_test, (p.x - 2,p.y +12))

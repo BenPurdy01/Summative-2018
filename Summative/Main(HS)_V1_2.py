45c405209f4ba2fd3210d7a2b47c4f3779f1c05c
BenPurdy01 <45369156+BenPurdy01@users.noreply.github.com>
2019-01-11 15:14:00 -0500

    Add files via upload

--- Summative/Main(HS)_V1_2.py
+++ Summative/Main(HS)_V1_2.py
@@ -23,6 +23,7 @@ class Player():
         self.Helth = 20
         self.Score = 0
         self.alive = True
+        self.curWep = 1
 
     def take_dmg(self):
         self.Helth = self.Helth - 1
@@ -88,40 +89,56 @@ def main():
     while running:
         max_zeds = 10+(5*wave_num) 
         screen.fill(GREEN)
-        if len(zed_list) < 7 and len(spawned_list) < max_zeds :
-            for we in range(7-(len(zed_list))):
+        """spawning zombies on screen"""
+        if len(zed_list) < (6 + wave_num) and len(spawned_list) < max_zeds :
+            for we in range((6 + wave_num)-(len(zed_list))):
                 temp_x = random.randint(0,screen_w)
                 temp_y = random.randint(0,screen_h)
                 zed_list.append(Zombie(temp_x,temp_y))
                 spawned_list.append(Zombie(temp_x,temp_y))
-
+        """Making the zombies not hit every frame"""
         for z in range(len(zed_list)):
             zed_list[z].isHit = False
         """This is the event handling, where the event queue is checked"""
+        """Weapon control"""
         for event in pygame.event.get():
+            """Getting the position of the mouse and the angle from the player"""
             if event.type == pygame.MOUSEMOTION:
                 mousex, mousey = event.pos
                 angle_1 = math.degrees(math.atan2(p.y-mousey, p.x-mousex))+180
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for z in range(len(zed_list)):
-                    if mousex >= zed_list[z].x and mousey >= zed_list[z].y and mousex <= (zed_list[z].x + 34) and mousey <= (zed_list[z].y + 56):
-                        zed_list[z].isHit = True
-
+            """controling the firerate of weapons"""
+            if p.curWep == 1 or p.curWep == 3:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for z in range(len(zed_list)):
+                        if mousex >= zed_list[z].x and mousey >= zed_list[z].y and mousex <= (zed_list[z].x + 34) and mousey <= (zed_list[z].y + 56):
+                            zed_list[z].isHit = True
+            if p.curWep == 2:
+                if pygame.mouse.get_pressed()[0] == True:
+                    for z in range(len(zed_list)):
+                        mp40_wait = time.time()
+                        if mousex >= zed_list[z].x and mousey >= zed_list[z].y and mousex <= (zed_list[z].x + 34) and mousey <= (zed_list[z].y + 56) and time.time() - mp40_wait < 0.4:
+                            zed_list[z].isHit = True
+                            mp40_wait = time.time()
+                            
+                
+            """Pause button"""
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    exit = False
                     screen.blit(pause_display, (230, 250))
+                    exit = False
                     while True:
                         for event in pygame.event.get():
                             if event.type == pygame.KEYDOWN:
                                 if event.key == pygame.K_ESCAPE:
                                     exit = True
                         if exit:
-                            break            
-            
+                            break
+                        
+            """Close the loop when you press the x"""
             if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
                 running = False
+
+        """Key control for walking/ weapon swaping"""        
         pause_display = game_font.render("Paused", False, (255,0,0))
         pressedkeys = pygame.key.get_pressed()
         pressedmouse = pygame.mouse.get_pressed()
@@ -146,14 +163,24 @@ def main():
                 p.y += p.sped
             elif (p.y + 56) >= screen_h:
                 p.y = p.y -10
+        if pressedkeys[pygame.K_1]:
+            p.curWep = 1
+        if pressedkeys[pygame.K_2]:
+            p.curWep = 2
+        if pressedkeys[pygame.K_3]:
+            p.curWep = 3
 
                 
 
-
+        """Load and scale the images"""
         image = pygame.image.load("player_noArm.png")
         image = pygame.transform.scale(image, (16,56))
-        gun_test = pygame.image.load("M9_v1.png")
-        gun_test = pygame.transform.scale(gun_test, (20,16))
+        wep_1 = pygame.image.load("M9_v1.png")
+        wep_1 = pygame.transform.scale(wep_1, (20,16))
+        wep_2 = pygame.image.load("mp40_v1.png")
+        wep_2 = pygame.transform.scale(wep_2, (32,18))
+        wep_3 = pygame.image.load("pump_v1.png")
+        wep_3 = pygame.transform.scale(wep_3, (52,10))
         zombie = pygame.image.load("zombie_0.png")
         zombie = pygame.transform.scale(zombie, (34,56))
         zombie_h = pygame.image.load("Zombie_hit.png")
@@ -166,15 +193,27 @@ def main():
         HP_Bar = pygame.transform.scale(HP_Bar, (114, 18))
         score_display = game_font.render(("Score:"+str(p.Score)), False, (255,255,255))
         wave_display = game_font.render(("Wave:"+str(wave_num)), False, (165,11,11))
-        if angle_1 > 270 or angle_1 < 90:
-          gun_test = pygame.transform.rotate(gun_test, angle_1)
-          gun_test = pygame.transform.flip(gun_test, 1,0)
-          image = pygame.transform.flip(image, 1,0)
-        else:
-          gun_test = pygame.transform.rotate(gun_test, -angle_1)
-          gun_test = pygame.transform.flip(gun_test, 1,1)
         CH = pygame.image.load("crosshair.png")
         CH = pygame.transform.scale(CH, (10,10))
+
+        """Which weapon is the player using"""
+        if p.curWep == 1:
+            current_weapon = wep_1
+        elif p.curWep == 2:
+            current_weapon = wep_2
+        elif p.curWep == 3:
+            current_weapon = wep_3
+
+        """flip and rotae the player and gun based off aim direction"""
+        if angle_1 > 270 or angle_1 < 90:
+          current_weapon = pygame.transform.rotate(current_weapon, angle_1)
+          current_weapon = pygame.transform.flip(current_weapon, 1,0)
+          image = pygame.transform.flip(image, 1,0)
+        else:
+          current_weapon = pygame.transform.rotate(current_weapon, -angle_1)
+          current_weapon = pygame.transform.flip(current_weapon, 1,1)
+
+        """Zombie controll in program"""
         new_zed_list = []
         for i in range(len(zed_list)):
             if zed_list[i].alive == True:
@@ -194,24 +233,37 @@ def main():
             wave_wait = time.time()
         if temp == False:            
             if time.time() - wave_wait >= 3:
-                print "1"
                 p.Score += max_zeds
                 wave_num += 1
                 spawned_list = []
                 ded_list = []
                 wave_wait = time.time()
                 temp = True
-        if angle_1 < 270 or angle_1 > 90:
+
+        """Drawing player"""
+        if angle_1 > 90 and angle_1 < 270:
           screen.blit(image, (p.x,p.y))
-          screen.blit(gun_test, (p.x - 2,p.y +12))
-        else:
+          if p.curWep == 1 or p.curWep == 2:
+              screen.blit(current_weapon, (p.x -15,p.y +12))
+          else:
+              screen.blit(current_weapon, (p.x -20,p.y +15))
+        elif angle_1 > 270 or angle_1 < 90:
           screen.blit(image, (p.x,p.y))
-          screen.blit(gun_test, (p.x +18,p.y +12))
+          if p.curWep == 1 or p.curWep == 2:
+              screen.blit(current_weapon, (p.x +10,p.y +12))
+          else:
+              screen.blit(current_weapon, (p.x -10,p.y +15))
+          
+        
+
+        """Is the player alive"""
         if p.Helth <= 0:
             p.alive = False
         if p.alive == False:
             screen.blit(G_O, (((screen_w/2)-50), ((screen_h/2)-50)))
+            screen.blit(score_display,(((screen_w/2)-50), ((screen_h/2)-30)))
             running = False
+        """Draw everything"""
         pygame.mouse.set_visible( False )
         screen.blit(CH, (mousex, mousey))
         screen.blit(ammo_counter,(120,450))
@@ -222,8 +274,7 @@ def main():
         screen.blit(wave_display,(0,0))
         pygame.display.flip()
         clock.tick(60)
-# run the main function only if this module is executed as the main script
-# (if you import this as a module then nothing is executed)
+"""This runs the main loop"""
 if __name__=="__main__":
-    # call the main function
+    """call the fuinction"""
     main()
